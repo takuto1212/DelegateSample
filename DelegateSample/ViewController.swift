8261e87d5a1dd20de029723b473b8ce012f9d7d7
西村　拓人 <takuto_nishimura@waku-2.com>
2018-09-05 14:12:01 +0900

    Initial Commit

---
+++ DelegateSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DelegateSample
+//
+//  Created by 西村　拓人 on 2018/09/05.
+//  Copyright © 2018年 西村　拓人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
